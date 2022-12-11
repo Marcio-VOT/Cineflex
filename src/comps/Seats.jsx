3f333da2fe728e61909bb6b32f850e2395dba139
MarcioJrPC <marciofsantos65@gmail.com>
2022-12-11 18:53:02 -0300

    post dados

--- src/comps/Seats.jsx
+++ src/comps/Seats.jsx
@@ -5,19 +5,38 @@ import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default () => {
-  const { idSessao } = useParams();
-  const [seats, setSeats] = useState(undefined);
-
   useEffect(() => {
     const promisse = axios.get(
       `https://mock-api.driven.com.br/api/v8/cineflex/showtimes/${idSessao}/seats`
     );
     promisse.then((resposta) => setSeats(resposta.data));
   }, []);
+  const { idSessao } = useParams();
+  const [seats, setSeats] = useState(undefined);
+  const [assento, setAssento] = useState([]);
+  const [name, setName] = useState("");
+  const [cpf, setCpf] = useState(undefined);
+  const navigate = useNavigate();
+
+  function reservSeats(event) {
+    event.preventDefault();
+    const promisse = axios.post(
+      "https://mock-api.driven.com.br/api/v8/cineflex/seats/book-many",
+      {
+        ids: assento,
+        name: name,
+        cpf: cpf,
+      }
+    );
+    promisse.then(() => navigate("/sucesso"));
+    promisse.catch(() => console.log("deu merda mané"));
+  }
 
   if (seats === undefined) return <div>carregando</div>;
+
   return (
     <>
       <Container>
@@ -25,26 +44,37 @@ export default () => {
         <SeatsContainer>
           {seats.seats.map((a) => {
             return (
-              <SeatsStyle disponivel={a.isAvailable} key={a.id}>
+              <SeatsStyle
+                cor={assento.includes(a.id) ? "lightgreen" : "ligthgray"}
+                disponivel={a.isAvailable}
+                key={a.id}
+                onClick={() => {
+                  assento.includes(a.id)
+                    ? setAssento(assento.filter((id) => id != a.id))
+                    : setAssento([...assento, a.id]);
+                }}
+              >
                 {a.name}
               </SeatsStyle>
             );
           })}
         </SeatsContainer>
-        <SeatsContainer>
-          <div>
-            <SeatsStyle style={{ background: "green" }}></SeatsStyle>
-            <p>selecionado</p>
-          </div>
-          <div>
-            <SeatsStyle></SeatsStyle>
-            <p>disponível</p>
-          </div>
-          <div>
-            <SeatsStyle style={{ background: "yellow" }}></SeatsStyle>
-            <p>indisponível</p>
-          </div>
-        </SeatsContainer>
+        <Guide />
+        <form onSubmit={reservSeats}>
+          <input
+            type="text"
+            required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="number"
+            required
+            value={cpf}
+            onChange={(e) => setCpf(e.target.value)}
+          />
+          <button type="submit">Reservat assento(s)</button>
+        </form>
       </Container>
       <Footer
         posterURL={seats.movie.posterURL}
@@ -54,6 +84,24 @@ export default () => {
     </>
   );
 };
+function Guide() {
+  return (
+    <SeatsContainer>
+      <div>
+        <SeatsStyle style={{ background: "green" }}></SeatsStyle>
+        <p>selecionado</p>
+      </div>
+      <div>
+        <SeatsStyle style={{ background: "lightgray" }}></SeatsStyle>
+        <p>disponível</p>
+      </div>
+      <div>
+        <SeatsStyle style={{ background: "yellow" }}></SeatsStyle>
+        <p>indisponível</p>
+      </div>
+    </SeatsContainer>
+  );
+}
 
 const Container = styled.div`
   display: flex;
@@ -86,7 +134,7 @@ const SeatsStyle = styled.button`
   width: 26px;
   height: 26px;
   border-radius: 50%;
-  background-color: ${(props) =>
-    props.disponivel ? "lightgreen" : "lightyellow"}
   border-style: none;
+  background-color: ${(props) =>
+    props.disponivel ? props.cor : "lightyellow"};
 `;
